53e40861a33d702c4890701dd52aff476ddb49f2
郭茂杰 <maojie_guo@techvision.com.cn>
2020-08-15 16:30:30 +0800

    加入shell script编译脚本

--- 001_一个文件调用另外两个文件中的函数/debug.sh
+++ 001_一个文件调用另外两个文件中的函数/debug.sh
@@ -5,7 +5,11 @@
 #       >时间：2020年08月14日 15时01分24秒
 #*********************************************************************************
 #!/bin/bash
-gcc -c increment.c
-gcc -c negate.c
-gcc -c main.c
-gcc -o run main.o increment.o negate.o
+echo 删除编译出的文件
+rm -rf *.o run
+echo 编译文件
+gcc -o run increment.c negate.c main.c
+echo 运行文件
+echo ====================
+echo
+./run
